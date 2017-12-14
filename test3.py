278df8e5cb3ebadf31ed644a653072f766347e4d
Philippe Lawson <philippe.lawson@lawson-it.com>
2017-12-14 04:04:56 +0100

    Synchronizing

--- test3.py
+++ test3.py
@@ -1,19 +1,20 @@
 import boto3
-from pyspark import SparkContext
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.classification import SVMWithSGD
+# from io import StringIO
 from io import BytesIO
 import numpy as np
 import sys
+import os
+import uuid
 import json
+import shutil
 import argparse
 
 
-sc = SparkContext(appName="animals_identification")
+# sc = SparkContext(appName="animals_identification")
 
 
 def create_features(config):
@@ -41,78 +42,19 @@ def create_features(config):
             .put(Body=breed_name + ',' + str(num_file) + ',' + json.dumps(features.tolist()[0])[1:][:-1])
 
 
-def create_features2(config):
-    s3 = boto3.resource('s3')
-    base_model = VGG16(weights='imagenet')  # download the keras model
-    model = Model(input=base_model.input, output=base_model.get_layer('fc2').output)
-    bucket = s3.Bucket(config['bucket'])
-
-    for obj in bucket.objects.filter(Prefix=config['image_key']):  # Read all jpeg files for the bucket key
-        try:
-            target_size = (224, 224)
-            img = image.pil_image.open(BytesIO(obj.get()['Body'].read()))
-            img = img.resize(target_size)
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            x = preprocess_input(x)
-            features = model.predict(x)  # Generate the features
-            # Save the features to a .txt file
-            pos_last_slash = len(obj.key) - obj.key[::-1].find("/")
-            filename = obj.key[pos_last_slash:]
-            suffix = ".jpg"
-            len_suffix = len(suffix)
-            pos_last_underscore = len(filename) - filename[::-1].find("_")
-            num_file = int(filename[pos_last_underscore:-len_suffix])
-            breed_name = filename[:(pos_last_underscore - 1)]
-            s3.Object(config['bucket'], config['features_key'] + config['sep']
-                      + obj.key[(len(obj.key) - obj.key[::-1].find("/")):] + ".txt") \
-                .put(Body=breed_name + ',' + str(num_file) + ',' + json.dumps(features.tolist()[0])[1:][:-1])
-        except IndexError:
-            print("====================================> IndexError for {}".format(obj.key))
-        except ValueError:
-            print("====================================> ValueError for {}".format(obj.key))
-
-
-def make_labeled_point(elements):
-    values = [float(x.strip()) for x in elements]
-    return LabeledPoint(values[0], values[1:])
-
-
 def s3_object_exists(bucket, file):
     client = boto3.client('s3')
     results = client.list_objects(Bucket=bucket, Prefix=file)
     return 'Contents' in results
 
 
-def do_1vs1(class_one, class_two, size, num_iter, config):
-    features_path = config['protocol'] + config['bucket'] + config['sep'] + config['features_key']
-    rdd_all = sc.textFile(features_path).map(lambda line: line.split(',')).persist()
-    rdd_train_set = rdd_all.filter(lambda elements: int(elements[1]) <= size and (elements[0] == class_one
-                                   or elements[0] == class_two)) \
-        .map(lambda elements: ['0.0' if elements[0] == class_one else '1.0'] + elements[2:]) \
-        .map(make_labeled_point)
-
-    rdd_test_set = rdd_all.filter(lambda elements: int(elements[1]) > size and (elements[0] == class_one
-                                    or elements[0] == class_two)) \
-        .map(lambda elements: ['0.0' if elements[0] == class_one else '1.0'] + elements[2:]) \
-        .map(make_labeled_point)
-
-    # Build the model
-    model = SVMWithSGD.train(rdd_train_set, iterations=num_iter)
-
-    # Evaluate the model on th test data
-    labels_and_preds = rdd_test_set.map(lambda p: (p.label, model.predict(p.features)))
-    train_err = labels_and_preds.filter(lambda lp: lp[0] != lp[1]).count() / float(rdd_test_set.count())
-    print("Test Error = " + str(train_err))
-
-
 def main():
     config = {
         "protocol": "s3://",
         "bucket": "oc-plawson",
-        "image_key": "distributed_learning/images",
+        "image_key": "distributed_learning/test_images",
         "sep": "/",
-        "features_key": "distributed_learning/features"
+        "features_key": "distributed_learning/test_features"
     }
 
     # Define command line parameters
@@ -152,10 +94,6 @@ def main():
     if not s3_object_exists(config['bucket'], config['features_key']):
         print('Creating all features...')
         create_features(config)
-
-    if None is not classx_classy:
-        print('Execuiting 1VS1')
-        do_1vs1(class_one, class_two, size, num_iter, config)
 
 
 if __name__ == "__main__":
